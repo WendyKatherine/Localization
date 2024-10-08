5606052cdca3b5a310281d536f654b20ed3e2fe6
Wendy Katherine Gomez <43182244+WendyKatherine@users.noreply.github.com>
2024-10-08 17:49:44 -0500

    Added the geolocation component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GeolocationComponent  from "./components/GeolocationComponent "
+
+function App() {
+  
+  return (
+    <div className="bg-gray-dark p-4">
+      <GeolocationComponent />
+    </div>
+  )
+}
+
+export default App
